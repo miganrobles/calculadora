d75e68a50573746351a36259b10f371091b16715
miguel <miganrobles@gmail.com>
2016-01-21 11:08:38 +0100

    ​Creado nuevo método que muestra por pantalla la suma de los números entre el 0 y el 10(ambos incluídos)

--- Calculadora.java
+++ Calculadora.java
@@ -10,11 +10,32 @@ public class Calculadora
     /**
      * Este método imprime por pantalla todos los mútiplos de 5 entre 10 y 95 (ambos no incluídos)
      */
-    public void multiplesOfFive() {
+    public void multiplesOfFive() 
+    {
         int num = 10;
+        boolean cabecera = true;
         while (num < 90) {
             num += 5;
+            if (cabecera) {
+                System.out.println("Los mútiplos de 5 entre 10 y 95 (ambos no incluídos) son:");
+                cabecera = false;
+            } 
             System.out.println(num);
         }
+    }
+    
+    /**
+     * Este métod no muestra por pantalla la suma de los números entre el 0 y el 10(ambos incluídos)
+     */
+    public void sumValues()
+    {
+        int num = 0;
+        int suma = 0;
+        while (num < 10) {
+            num++;
+            suma += num;
+        }
+        System.out.println("La suma de los números entre el 0 y el 10(ambos incluídos) es:");
+        System.out.println(suma);
     }
 }
